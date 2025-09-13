76d9d032e330ec96682c92a6c31201f78465f007
teocot <teodor@xtra.co.nz>
2025-09-13 21:22:03 +1200

    merge

---
+++ Services/Interfaces/IPurchaseService.cs
@@ -0,0 +1,17 @@
+﻿using CarAPI.Models;
+
+namespace CarAPI.Services.Interfaces
+{
+    public interface IPurchaseService
+    {
+        Task<List<Purchase>> GetAllAsync();
+        Task<Purchase?> GetByIdAsync(int id);
+        Task<bool> CreateAsync(PurchaseViewModel model);
+        Task<bool> UpdateAsync(int id, PurchaseViewModel model);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+        Task<List<Person>> GetPeopleAsync();
+        Task<List<Car>> GetAvailableCarsAsync();
+        Task<Car> GetCarByIdAsync(int carId);
+    }
+}
---
+++ Services/PurchaseService.cs
@@ -0,0 +1,86 @@
+﻿using CarAPI.Data;
+using CarAPI.Models;
+using CarAPI.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarAPI.Services
+{
+    public class PurchaseService : IPurchaseService
+    {
+        private readonly AppDbContext _context;
+
+        public PurchaseService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Purchase>> GetAllAsync()
+        {
+            return await _context.Purchases.Include(p => p.Buyer).Include(p => p.Car).ToListAsync();
+        }
+
+        public async Task<Purchase?> GetByIdAsync(int id)
+        {
+            return await _context.Purchases.Include(p => p.Buyer).Include(p => p.Car).FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<bool> CreateAsync(PurchaseViewModel model)
+        {
+            var purchase = new Purchase
+            {
+                PurchaseDate = model.PurchaseDate,
+                PersonId = model.PersonId,
+                CarId = model.CarId
+            };
+
+            _context.Purchases.Add(purchase);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> UpdateAsync(int id, PurchaseViewModel model)
+        {
+            var purchase = await _context.Purchases.FindAsync(id);
+            if (purchase == null) return false;
+
+            purchase.PurchaseDate = model.PurchaseDate;
+            purchase.PersonId = model.PersonId;
+            purchase.CarId = model.CarId;
+
+            _context.Update(purchase);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var purchase = await _context.Purchases.FindAsync(id);
+            if (purchase == null) return false;
+
+            _context.Purchases.Remove(purchase);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> ExistsAsync(int id)
+        {
+            return await _context.Purchases.AnyAsync(p => p.Id == id);
+        }
+
+        public async Task<List<Person>> GetPeopleAsync()
+        {
+            return await _context.People.ToListAsync();
+        }
+
+        public async Task<List<Car>> GetAvailableCarsAsync()
+        {
+            return await _context.Cars.Where(c => c.Purchase == null).ToListAsync();
+        }
+
+        public async Task<Car?> GetCarByIdAsync(int carId)
+        {
+            return await _context.Cars.FindAsync(carId);
+        }
+
+    }
+}
